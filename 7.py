45a5573764df71ccfdf6f7f4bd0b283af9bdd069
knoxyys <stirlingthebean@gmail.com>
2024-12-04 12:00:11 +1100

    finish 7

--- 7.py
+++ 7.py
@@ -1,3 +1,5 @@
 length = int(input("What is the length of the room in feet? "))
 width = int(input("What is the width of the room in feet? "))
 print(f"You entered dimensions of {length} feet by {width} feet.\nThe area is")
+print(f"{length * width} square feet")
+print(f"{(length * width) * 0.09290304} square metres")
