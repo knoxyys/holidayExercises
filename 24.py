952b53b223068e72e8ad9b1f757825bf284e6519
knoxyys <stirlingthebean@gmail.com>
2024-12-11 11:31:19 +1100

    actually fixed 24

--- 24.py
+++ 24.py
@@ -1,7 +1,7 @@
 #  This chapter is intentionally short, because when you finish these exercises, you should revisit your previous programs and see how functions can improve the organization of those programs as well (gulp)
 
 def isAnagram(word1, word2):
-    if set(word1) == set(word2) and len(word1) == len(word2): # set ignores order of items but ignores duplication, len fixes this
+    if sorted(word1) == sorted(word2):
         return True
     else: return False
 
