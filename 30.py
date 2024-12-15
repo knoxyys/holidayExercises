7ad39bb823de66910c608c1fff867a0f74ac9117
knoxyys <stirlingthebean@gmail.com>
2024-12-16 07:01:35 +1100

    coment

--- 30.py
+++ 30.py
@@ -1,3 +1,3 @@
 for i in range(13):
-    for ii in range(13):
+    for ii in range(13): # goes from 0 to 12, then increases i by one and repeats until i = 12
         print(f"{i} x {ii} = {i * ii}")
