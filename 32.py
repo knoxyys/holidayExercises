cfff4c7b4862ac4ae8c0085c8a74e6c970c68d22
knoxyys <stirlingthebean@gmail.com>
2025-01-03 00:23:53 +1100

    rewrote 32 bc old one bad

--- 32.py
+++ 32.py
@@ -1,38 +1,32 @@
-import time
 import random
 
-def validIntInput(x): # stealing same function from before bc its useful
-    while True:
-        try:
-            return int(input(x))
-        except ValueError:
-            continue
+print("Let's play guess the number.")
 
-def game():
-    diff = validIntInput("Let's play guess the number.\nPick a difficulty level (1, 2 or 3): ")
-    if diff > 3:
-        time.sleep(99999999) # lol
-
-    number = random.randint(0, 10 ** diff)
-
-    counter = 1
-    guess = int(input("I have your number. What's your guess? "))
+def playGame():
+    diff = input('Pick a difficulty level: (1, 2, or 3): ')
+    while diff not in ['1', '2', '3']:
+        diff = input('Choice needs to be in: (1, 2, or 3): ')
+        
+    number = random.randint(1, 10 ** int(diff))
+    
+    count = 1
+    
+    guess = int(input("I have my number. What's your guess? "))
     while guess != number:
-        if guess > number:
-            guess = int(input("Too high. Guess again: "))
-            counter = counter + 1
+        if guess < number:
+            guess = int(input("Too low! Try again: "))
+            count = count + 1
         else:
-            guess = int(input("Too low. Guess again: "))
-            counter = counter + 1
+            guess = int(input("Too high! Try again: "))
+            count = count + 1
 
-    print(f"The number was {number}. You got it in {counter} guesses!")
-
-while True:
-    game()
-    while True:
+    print(f"You got it in {count} guesses!")
+    
+    play = ''
+    while play not in ['y', 'n']:
         play = input("Play again? ")
         if play == 'y':
-            break
-        elif play == 'n':
-            print("Goodbye! ")
-            quit()
+            playGame()
+        else: break
+
+playGame()
