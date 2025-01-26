ae778c78aa64483ab6c41bee231634c6289d746b
knoxyys <stirlingthebean@gmail.com>
2025-01-26 13:09:07 +1100

    fixed 48 with another api

--- 48.py
+++ 48.py
@@ -1,17 +1,26 @@
 import requests
 import os
 import json
-lat = -33.865143
-lon = 151.209900
 apikey = os.getenv('48_API_KEY')
 
-response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apikey}')
-if response.status_code == 200:
-    data = json.loads(response.content)
+loc = input("Where are you? (Name of city) ")
+locapi = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={loc}&limit=5&appid={apikey}') # uses other api to get coordinates from name
+if locapi.status_code == 200:
+    locdata = json.loads(locapi.content)
+    lat = locdata[0]['lat']
+    lon = locdata[0]['lon']
 else:
-    print('Data not found')
+    print('Coordinate data not found')
 
-kelvin_temp = data['main']['temp']
 
-print(f"Weather in {data['name']}:")
+
+weatherapi = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apikey}')
+if weatherapi.status_code == 200:
+    weather_data = json.loads(weatherapi.content)
+else:
+    print('Weather data not found')
+
+kelvin_temp = weather_data['main']['temp']
+
+print(f"Weather in {weather_data['name']}:")
 print(f"{kelvin_temp - 273.15:.2f} degrees celcius.")
